e9be65beabc24ed486b29159d6f9fbcfc345bad1
arpitklu <arrpiit2004@gmail.com>
2025-07-30 13:06:10 +0530

    HashMap : Finding Duplicate Element | Medium (63.1) | Leetcode

---
+++ Arrays/Find_Duplicate_Elements.java
@@ -0,0 +1,67 @@
+/*
+287. Find the Duplicate Number
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+
+There is only one repeated number in nums, return this repeated number.
+
+You must solve the problem without modifying the array nums and using only constant extra space.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3,4,2,2]
+Output: 2
+Example 2:
+
+Input: nums = [3,1,3,4,2]
+Output: 3
+Example 3:
+
+Input: nums = [3,3,3,3,3]
+Output: 3
+ 
+
+Constraints:
+
+1 <= n <= 105
+nums.length == n + 1
+1 <= nums[i] <= n
+All the integers in nums appear only once except for precisely one integer which appears two or more times.
+ 
+
+Follow up:
+
+How can we prove that at least one duplicate number must exist in nums?
+Can you solve the problem in linear runtime complexity?
+
+*/
+
+
+class Solution {
+    public int findDuplicate(int[] nums) {
+        //intialise
+        HashMap<Integer,Integer>map = new HashMap<>();
+        //add
+        for(int n:nums){
+            if(!map.containsKey(n)){
+                map.put(n,1);
+            }
+            else{
+                map.put(n,map.get(n)+1);
+            }
+        }
+        //iterate
+        for(int key:map.keySet()){
+            if(map.get(key)>1){
+                return key;
+            }
+        }
+        return -1;
+    }
+}
